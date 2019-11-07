b0aeef994b89d66a187bd396ac0b2b862df9a87f
dipadana <putudipadana@gmail.com>
2019-11-07 21:19:08 +0700

    Udpate Components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
+  <div>
   
+    <Navbar></Navbar>
+
+    <Home v-show="page == 'home'"></Home>
+
+  </div>
 </template>
 
 <script>
-export default {
+import Navbar from './components/Navbar'
+import Home from './views/Home'
 
+export default {
+  data(){
+    return{
+      page: 'home'
+    }
+  },
+  components:{
+    Navbar,
+    Home
+  }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=McLaren&display=swap')
+
 
 </style>
